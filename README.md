e20173f4af983ee56e122f10923a413cdb977a93
Motz <motzumoto@lunardev.group>
2023-08-12 04:06:58 -0400

    Upload

---
+++ README.md
@@ -0,0 +1,57 @@
+# Steam Workshop URL Parser
+
+A Python tool to extract Mod IDs and Workshop IDs from Steam Workshop URLs.
+
+## Introduction
+
+This tool is designed for users who want to easily retrieve Mod IDs and Workshop IDs from provided Steam Workshop URLs. It's particularly handy if you frequently work with mods and want a quick way to get these identifiers.
+
+## Features
+
+- Extract Mod IDs and Workshop IDs from given URLs.
+- User-friendly command-line interface.
+- Handles request errors gracefully.
+  
+## Requirements
+
+- Python 3
+- `requests` and `beautifulsoup4` libraries
+
+## Installation
+
+1. Clone the repository:
+   ```
+   git clone https://github.com/Motzumoto/steam-workshop-url-parser.git
+   ```
+
+2. Navigate to the project directory:
+   ```
+   cd steam-workshop-url-parser
+   ```
+
+3. Install required libraries:
+   ```
+   pip install requests beautifulsoup4
+   ```
+
+## Usage
+
+1. Run the script:
+   ```
+   python steam_workshop_parser.py
+   ```
+
+2. Follow the on-screen prompts to input your Steam Workshop URLs.
+
+3. Type `DONE` when you're finished, and the script will display the extracted Mod IDs and Workshop IDs.
+
+## Contributions
+
+Pull requests are welcome! For major changes, please open an issue first to discuss what you'd like to change.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://opensource.org/licenses/MIT
+) file for details.
+
+---
---
+++ workshop_url_parser.py
@@ -0,0 +1,74 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+def fetch_and_parse(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+
+    try:
+        return extract_mod_and_workshop_ids(url, headers)
+    except requests.RequestException as e:
+        print(f"Error fetching {url}. Error: {str(e)}")
+        return [], []
+
+    except Exception as e:
+        print(f"General error for {url}. Error: {str(e)}")
+        return [], []
+
+def extract_mod_and_workshop_ids(url, headers):
+    response = requests.get(url, headers=headers, timeout=10)
+    if response.status_code != 200:
+        print(f"Failed to fetch {url} with status code: {response.status_code}")
+        return [], []
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    description = soup.select_one(".workshopItemDescription").text
+
+    mod_ids = []
+    workshop_ids = []
+
+    lines = description.split("\n")
+    for line in lines:
+        line = line.strip()
+        if "Mod ID:" in line:
+            mod_id = line.split(":")[-1].strip()
+            mod_ids.append(mod_id)
+        elif "Workshop ID:" in line:
+            workshop_id = line.split(":")[-1].strip()
+            if workshop_id.isdigit():
+                workshop_ids.append(workshop_id)
+
+    # In case the Workshop ID isn't in the description
+    if not workshop_ids:
+        workshop_id_from_url = url.split("?id=")[-1]
+        workshop_ids.append(workshop_id_from_url)
+
+    return workshop_ids, mod_ids
+
+def main():
+    print("Welcome to the Steam Workshop URL Parser!")
+    print("Enter the mod URLs one by one. Type 'DONE' when you have finished entering all the URLs.")
+    urls = []
+
+    while True:
+        url = input("Enter a mod URL: ")
+        if url == "DONE":
+            break
+        urls.append(url)
+
+    all_mod_ids = []
+    all_workshop_ids = []
+
+    for url in urls:
+        workshop_ids, mod_ids = fetch_and_parse(url)
+        all_mod_ids.extend(mod_ids)
+        all_workshop_ids.extend(workshop_ids)
+        time.sleep(1)  # 1-second delay between requests
+
+    print("\nParsed Information:")
+    print("Mods=" + ";".join(all_mod_ids))
+    print("WorkshopItems=" + ";".join(all_workshop_ids))
+
+if __name__ == "__main__":
+    main()
